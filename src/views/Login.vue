119c44a32cd7208b14d9d36391fda6761e9c387f
ChengHaoHappy <31426319+ChengHaoHappy@users.noreply.github.com>
2020-03-18 17:40:36 +0800

    添加验证码

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        <el-form :rules="rules" :model="loginForm" ref="loginForm" class="loginContainer">
+        <el-form
+                :rules="rules"
+                :model="loginForm"
+                v-loading="loading"
+                ref="loginForm"
+                class="loginContainer">
             <h3 class="loginTitle">系统登录</h3>
             <el-form-item prop="username">
                 <el-input type="text" v-model="loginForm.username" auto-complete="off"
@@ -8,7 +13,12 @@
             </el-form-item>
             <el-form-item prop="password">
                 <el-input v-model="loginForm.password" auto-complete="off" aria-placeholder="请输入密码"
-                    @keydown.enter.native="submitLogin"></el-input>  <!-- @keydown.enter.native 回车登录-->
+                         ></el-input>
+            <el-form-item>
+                <el-input size="normal" type="text" v-model="loginForm.code" auto-complete="off"
+                          placeholder="点击图片更换验证码" @keydown.enter.native="submitLogin" style="width: 250px"></el-input>
+                <img :src="vcUrl" @click="updateVerifyCode" alt="" style="cursor: pointer"> <!-- @keydown.enter.native 回车登录-->
+            </el-form-item>
             </el-form-item>
             <el-checkbox class="loginRemember" v-model="checked"></el-checkbox>
             <el-button type="primary" style="width: 100%" @click="submitLogin()">登录</el-button>
@@ -22,29 +32,39 @@
         name: "Login",
         data() {
             return {
+                loading: false,
+                vcUrl: '/verifyCode?time='+new Date(),
                 loginForm: {
                     username: 'admin',
-                    password: '123'
+                    password: '123',
+                    code:''
                 },
                 checked: true,
                 rules: {
                     username: [{required: true, message: '请输入用户名', trigger: 'blur'}],
-                    password: [{required: true, message: '请输入密码', trigger: 'blur'}]
+                    password: [{required: true, message: '请输入密码', trigger: 'blur'}],
+                    code: [{required: true, message: '请输入验证码', trigger: 'blur'}]
                 }
             }
         },
         methods: {
+            updateVerifyCode() {
+                this.vcUrl = '/verifyCode?time='+new Date();
+            },
             submitLogin() {
                 this.$refs.loginForm.validate((valid) => {
+                    this.loading = true;
                     if (valid) {
                         // alert('submit!');
                         this.postKeyValueRequest('/doLogin', this.loginForm).then(resp => { //服务端返回的参数，respBean的json字符串
+                            this.loading = false;
                             if (resp) {  //登录成功
-                                // alert(JSON.stringify(resp));
-                                this.$store.commit('INIT_CURRENTHR', resp.obj);
-                                window.sessionStorage.setItem("user",JSON.stringify(resp.obj)); //把用户数据存储到session
+                                this.$store.commit('INIT_CURRENTHR', resp.obj);  //触发WebSocket的action
+                                window.sessionStorage.setItem("user", JSON.stringify(resp.obj)); //把用户数据存储到session
                                 let path = this.$route.query.redirect;
-                                this.$router.replace((path =='/' || path == undefined)?'/home':path);
+                                this.$router.replace((path == '/' || path == undefined) ? '/home' : path);
+                            }else{
+                                this.vcUrl = '/verifyCode?time='+new Date();
                             }
                         })
                     } else { //登录失败
